075cebd73804873d801f3f627c661719c45b07f4
arifaslam2002 <aarifaslam2002@email.com>
2026-09-21 11:26:34 +0530

    Update Port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
